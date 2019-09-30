d4177054d394541e40af2863703ad2e1ee55df00
dissfall <dissfalling@gmail.com>
2019-09-30 15:39:43 +0300

    feat(app): make route to main view

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+
+import MainView from './views/main';
+
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <h1>hi</h1>
-    </div>
+    <Router>
+      <Route exact path="/" component={MainView} />
+    </Router>
   );
 }
 
